f6a1a5e133d2f46df7f9d21d2bba6efeb5bc5d8b
myo-i <Imiyo0521@gmail.com>
2020-10-08 09:43:32 +0900

    商品詳細機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def index
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
@@ -14,6 +15,16 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @user = @item.user
+    @category = Genre.find(@item.category_id)
+    @state = State.find(@item.product_state_id)
+    @burden = Burden.find(@item.payment_burden_id)
+    @area = Area.find(@item.area_id)
+    @days = Day.find(@item.delivery_days_id)
   end
 
   private
